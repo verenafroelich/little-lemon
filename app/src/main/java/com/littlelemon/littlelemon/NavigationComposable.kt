97c36c4570e03a4026703c9dba4a5c1a27c69ba0
verena <verena-froelich@web.de>
2025-05-21 11:58:40 +0200

    Erfolgreiche Onboarding-Seite

--- app/src/main/java/com/littlelemon/littlelemon/NavigationComposable.kt
+++ app/src/main/java/com/littlelemon/littlelemon/NavigationComposable.kt
@@ -1,19 +1,21 @@
 package com.littlelemon.littlelemon
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
 
 
 @Composable
 fun navigation(navController: NavHostController) {
-
+    var userLoggedIn = false //TODO: Use SharedReference "userLoggedIn"
+    var startDestination = Onboarding.route
+    if(userLoggedIn){
+        startDestination = Home.route
+    }
     NavHost(
         navController = navController,
-        startDestination = Onboarding.route
+        startDestination = startDestination
         //startDestination = if (user is logged)
         //{Onboarding.route} else {Home.route}
     ) {
