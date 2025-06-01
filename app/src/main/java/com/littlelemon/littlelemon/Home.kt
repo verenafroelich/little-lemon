335659eb651065b75bdab58a66f3ab21dd285ff0
verena <verena-froelich@web.de>
2025-06-01 13:19:44 +0200

    test commit after crash

--- app/src/main/java/com/littlelemon/littlelemon/Home.kt
+++ app/src/main/java/com/littlelemon/littlelemon/Home.kt
@@ -1,11 +1,27 @@
 package com.littlelemon.littlelemon
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Home() {
-
+    Row {
+        Image(
+            painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .fillMaxWidth(.32f)
+                .fillMaxHeight(.12f)
+        )
+        Image(
+            painterResource(id = R.drawable.profile),
+            contentDescription = "Profile",
+            modifier = Modifier
+                .fillMaxWidth(.32f)
+                .fillMaxHeight(.12f)
+        )
+    }
 }
 
 @Preview(showBackground = true)
